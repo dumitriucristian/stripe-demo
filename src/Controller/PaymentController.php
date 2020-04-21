36c0e17ae00b0ebd680413b42534679d884e1957
cristian.dumitriu <dumitriucristian@yahoo.com>
2020-04-21 21:33:02 +0300

    add payment service

--- src/Controller/PaymentController.php
+++ src/Controller/PaymentController.php
@@ -4,52 +4,57 @@
 namespace App\Controller;
 
 
+use App\Service\StripeSource;
+use App\Service\TestService;
 use Stripe\Stripe;
 use Stripe\Source;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 
 class PaymentController extends AbstractController
 {
+    private $source;
+    private $token;
 
+    public function __construct( StripeSource $stripe)
+    {
+        $this->source =  $stripe->create();
+    }
 
     public function index()
     {
        return  $this->render('index.html.twig');
     }
 
+    private function getToken()
+    {
+        return $this->source->klarna->client_token;
+    }
+
+    private function getPaymentMethods()
+    {
+        $availablePaymentMethods = $this->source->klarna->payment_method_categories;
+        $paymentMethods = $availablePaymentMethods ? explode(',', $availablePaymentMethods) : '';
+        return $paymentMethods;
+    }
 
     public function shop()
     {
-        Stripe::setApiKey('pk_test_OMTgzDiFsMbggYdXLsLJg3vr00qoclCHNC');
-        $source = Source::create([
-            "type" => "klarna",
-            "currency" => "usd",
-            "amount" => 10000,
-            'klarna' => [
-                'product' => 'payment',
-                'purchase_country' => 'US',
-                'custom_payment_methods' => 'payin4,installments'
-            ],
-            'source_order' => [
-                'items' => [
-                    0 => [
-                        'type' => 'sku',
-                        'quantity' => 1,
-                        'amount' => 10000,
-                        'currency' => 'usd',
-                        'description' => 'Grey cotton shirt'
-                    ]
-                ]
-            ]
-        ]);
-        dd($source);
-        $availablePaymentMethods = $source->klarna->payment_method_categories;
-        $paymentMethods = $availablePaymentMethods ? explode(',', $availablePaymentMethods) : '';
-
-        return $this->render('shop.html.twig',
-        [
-            'paymentMethods' => $paymentMethods
-        ]
-    );
+        return $this->render('shop.html.twig', ['paymentMethods' => $this->getPaymentMethods()]);
     }
+
+    public function authorize(Request $request)
+    {
+        $paymentType = $request->get('paymentType');
+        $paymentToken = $this->getToken();
+
+        return $this->render('authorize.html.twig', [
+            'paymentType' => $paymentType,
+            'paymentToken' => $paymentToken
+        ]);
+    }
+
+
+
+
 }
--- src/Service/StripeSource.php
+++ src/Service/StripeSource.php
@@ -4,15 +4,45 @@
 namespace App\Service;
 
 
+use Stripe\Source;
 use Stripe\Stripe;
 
 class StripeSource
 {
+    private $order;
 
     public function __construct()
     {
-        $stripe = Stripe::setApiKey('pk_test_OMTgzDiFsMbggYdXLsLJg3vr00qoclCHNC');
+        $this->order = [
+            "type" => "klarna",
+            "currency" => "usd",
+            "amount" => 10000,
+            'klarna' => [
+                'product' => 'payment',
+                'purchase_country' => 'US',
+                'custom_payment_methods' => 'payin4,installments'
+            ],
+            'source_order' => [
+                'items' => [
+                    0 => [
+                        'type' => 'sku',
+                        'quantity' => 1,
+                        'amount' => 10000,
+                        'currency' => 'usd',
+                        'description' => 'Grey cotton shirt'
+                    ]
+                ]
+            ]
+        ];
     }
+
+    public function create()
+    {
+        Stripe::setApiKey('pk_test_OMTgzDiFsMbggYdXLsLJg3vr00qoclCHNC');
+        return   $source = Source::create($this->order);
+    }
+
+
 
 
 }
